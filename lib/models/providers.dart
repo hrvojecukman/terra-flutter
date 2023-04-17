55d9a182001a9c1d5d797c5caff20f58275ae5cd
hrvoje <hrvoje.cukman@gmail.com>
2023-04-17 13:49:13 +0200

    Added providers enum

---
+++ lib/models/providers.dart
@@ -0,0 +1,37 @@
+enum Provider {
+  fitbit('FITBIT'),
+  coros('COROS'),
+  dexcom('DEXCOM'),
+  oura('OURA'),
+  trainingPeaks('TRAININGPEAKS'),
+  withings('WITHINGS'),
+  suunto('SUUNTO'),
+  peloton('PELOTON'),
+  zwift('ZWIFT'),
+  garmin('GARMIN'),
+  eightSleep('EIGHT'),
+  wahoo('WAHOO'),
+  googleFit('GOOGLE'),
+  polar('POLAR'),
+  pul('PUL'),
+  appleHealthKit('APPLE'),
+  samsung('SAMSUNG'),
+  wearOS('WEAROS'),
+  freestyleLibre('FREESTYLELIBRE'),
+  tempoFit('TEMPO'),
+  iFit('IFIT'),
+  concept('2	CONCEPT2'),
+  fatsecret('FATSECRET'),
+  cronometer('CRONOMETER'),
+  nutracheck('NUTRACHECK'),
+  underarmour('UNDERARMOUR'),
+  googleFitSDK('GOOGLEFIT'),
+  freestyleLibreSDK('FREESTYLELIBRESDK'),
+  omron('OMRON'),
+  renpho('RENPHO'),
+  whoop('WHOOP');
+
+  final String key;
+
+  const Provider(this.key);
+}
